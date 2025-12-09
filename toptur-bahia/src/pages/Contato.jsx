0acce87aae44b4bf1a298f1b56574fdab58a17c3
Fredson Maikon <fredsonsouzalemos@gmail.com>
2025-12-09 19:29:15 -0300

    logs add

--- toptur-bahia/src/pages/Contato.jsx
+++ toptur-bahia/src/pages/Contato.jsx
@@ -176,6 +176,11 @@ const MapBox = styled.div`
 `;
 
 export default function Contato() {
+
+  console.log("SERVICE:", import.meta.env.VITE_EMAILJS_SERVICE_ID);
+console.log("TEMPLATE:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
+console.log("KEY:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
   const [modalOpen, setModalOpen] = useState(false);
 
  function sendEmail(e) {
